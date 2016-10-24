a598c7873e2d2467e169245d5bdd8cbc96e174e7
Christoph Schürz <christoph.schuerz@boku.ac.at>
2016-10-24 19:36:22 +0200

    pasta party started...

---
+++ R/infix.R
@@ -0,0 +1,59 @@
+#' Paste function
+#'
+#' Pastes two strings by "_".
+#' @name \%_\%
+#' @param a a character string
+#' @param y a character string
+#' @export
+#' @rdname _
+"%_%" <- function(a, b) paste(a, b, sep = "_")
+
+#' Paste function
+#'
+#' Pastes two strings by "-".
+#' @name \%/\%
+#' @param a a character string
+#' @param y a character string
+#' @export
+#' @rdname /
+'%-%' <- function(a, b) paste(a, b, sep = "-")
+
+#' Paste function
+#'
+#' Pastes two strings by ".".
+#' @name \%.\%
+#' @param a a character string
+#' @param y a character string
+#' @export
+#' @rdname .
+'%.%' <- function(a, b) paste(a, b, sep = ".")
+
+#' Paste function
+#'
+#' Pastes two strings by "/".
+#' @name \%/\%
+#' @param a a character string
+#' @param y a character string
+#' @export
+#' @rdname /
+'%//%' <- function(a, b) paste(a, b, sep = "/")
+
+#' Paste function
+#'
+#' Pastes two strings by "".
+#' @name \%/\%
+#' @param a a character string
+#' @param y a character string
+#' @export
+#' @rdname /
+'%&%' <- function(a, b) paste0(a, b)
+
+#' Paste function
+#'
+#' Pastes two strings by " ".
+#' @name \%/\%
+#' @param a a character string
+#' @param y a character string
+#' @export
+#' @rdname /
+'%&&%' <- function(a, b) paste0(a, b)

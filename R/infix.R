847ea71358062ac8b91b6f2d4cba315065e48e63
Daniel Klotz <daniel.klotz@boku.ac.at>
2016-11-30 11:26:09 +0100

    Ready for the finishing touches
    organised the code in a new way + added examples.

--- R/infix.R
+++ R/infix.R
@@ -1,58 +1,62 @@
 #' Paste underscore Function
 #'
-#' Pastes two strings by "_".
-#' @param a a character string
-#' @param y a character string
+#' \%_\% pastes two strings by "_".
+#' @rdname pasta
 #' @export
-#' @examples
-#' 'spaghetti' %_% 'sauce'
-#' 'noodle' %_% 'soup'
 "%_%" <- function(a, b) paste(a, b, sep = "_")
 
 #' Paste hypen function
 #'
-#' Pastes two strings by "-".
-#' @param a a character string
-#' @param y a character string
+#' \%-\% pastes two strings by "-".
+#' @rdname pasta
 #' @export
 '%-%' <- function(a, b) paste(a, b, sep = "-")
 
 #' Paste dot functions
 #'
-#' Pastes two strings by ".".
-#' @param a a character string
-#' @param y a character string
+#' \%.\% pastes two strings by ".".
+#' @rdname pasta
 #' @export
 '%.%' <- function(a, b) paste(a, b, sep = ".")
 
 #' Paste slash function
 #'
-#' Pastes two strings by "/".
-#' @param a a character string
-#' @param y a character string
+#' \%//\% pastes two strings by "/".
+#' @rdname pasta
 #' @export
 '%//%' <- function(a, b) paste(a, b, sep = "/")
 
 #' Paste 0 function
 #'
-#' Pastes two strings by "".
-#' @param a a character string
-#' @param y a character string
+#' \%&\% pastes two strings by "".
+#' @rdname pasta
 #' @export
 '%&%' <- function(a, b) paste0(a, b)
 
 #' Paste space function
 #'
-#' Pastes two strings by " ".
-#' @param a a character string
-#' @param y a character string
+#' \%&&\% pastes two strings by " ".
+#' @rdname pasta
 #' @export
 '%&&%' <- function(a, b) paste(a, b, sep = " ")
 
 #' Paste or function
 #'
-#' Pastes two strings with "|".
-#' @param a a character string
-#' @param y a character string
+#' \%|\% pastes two strings with a "|".
+#' @rdname pasta
 #' @export
 '%|%' <- function(a, b) paste(a, b, sep = "|")
+
+#' Make pasta with strings
+#'
+#' @param a,b characters.
+#' @name pasta
+#' @examples
+#' 'spaghetti' %_% 'sauce' # 'spaghetti_sauce'
+#' 'ravioli' %-% 'recipe' # 'ravioli-recipe'
+#' 'spaghetti' %.% 'code' # 'spaghetti.code'
+#' 'fussili' %//% 'farfalle' # 'fussili/farfalle'
+#' 'pasta' %&% 'sciutta' # 'pastasciutta'
+#' 'noodle' %&&% 'soup' # 'noodle soup'
+#' 'carbonara' %|% 'ragù' # 'carbonara|ragù'
+NULL
